e10bde74584a9e9c27d00fd6c1c7f187341e7031
karunyavenu <karunyavenu01@gmail.com>
2026-08-17 22:08:22 +0530

    Add files via upload

---
+++ managing bookstore.sql
@@ -0,0 +1,12 @@
+UPDATE books SET price = price + 50, stock = 12 WHERE title = 'Learn SQL';
+UPDATE books SET stock = stock - 2 WHERE price > 500;
+ DELETE FROM books WHERE book_id = 3;  
+
+
+
+
+
+
+
+
+
