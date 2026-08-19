3ac30161ba1bcc112479402cf9427ae0519c637d
karunyavenu <karunyavenu01@gmail.com>
2026-08-19 09:42:08 +0530

    Add files via upload

---
+++ bookstore database.sql
@@ -0,0 +1,15 @@
+SELECT * FROM books WHERE price < 450 AND stock > 30;
+UPDATE books SET stock = 45 price = 420 WHERE title = 'Deep Work';
+DELETE FROM books WHERE title = 'Ikigai';
+SELECT AVG(price) AS average_price,COUNT(*) AS total_books FROM books;
+SELECT * FROM book ORDER BY price DESC LIMIT 3;
+
+
+       
+
+
+
+
+    
+
+
